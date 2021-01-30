175156abf50b8e752e1ba9eaac301c03993ecc33
Dwaipayan Munshi <53687927+dwaipayan05@users.noreply.github.com>
2021-01-31 00:58:01 +0530

    Added Countdown Timer

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -218,6 +218,41 @@
     });
   }
 
+  //Countdown
+  (function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  let birthday = "Mar 5, 2021 10:00:00",
+      countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {
+
+        let now = new Date().getTime(),
+            distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        if (distance < 0) {
+          let headline = document.getElementById("headline"),
+              countdown = document.getElementById("countdown"),
+              content = document.getElementById("countext");
+
+          headline.innerText = "CONVENTION DAY";
+          countdown.style.display = "none";
+          content.style.display = "block";
+
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
+
   // Page loading animation
   $(window).on("load", function() {
     if ($(".cover").length) {
