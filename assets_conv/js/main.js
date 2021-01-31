d0b772053cabc5ddd9a4755afe36eb31f679c339
Dwaipayan Munshi <53687927+dwaipayan05@users.noreply.github.com>
2021-02-01 02:06:51 +0530

    Edited Convetion 21 Page

--- assets_conv/js/main.js
+++ assets_conv/js/main.js
@@ -6,41 +6,41 @@ jQuery(document).ready(function($) {
             $(".seq-preloader").fadeOut(); // will first fade out the loading animation
             $(".sequence").delay(500).fadeOut("slow"); // will fade out the white DIV that covers the website.
         })
-      
-        
+
+
         $(function() {
-  
+
         function showSlide(n) {
             // n is relative position from current slide
-          
+
             // unbind event listener to prevent retriggering
             $body.unbind("mousewheel");
-          
+
             // increment slide number by n and keep within boundaries
             currSlide = Math.min(Math.max(0, currSlide + n), $slide.length-1);
-            
+
             var displacment = window.innerWidth*currSlide;
             // translate slides div across to appropriate slide
             $slides.css('transform', 'translateX(-' + displacment + 'px)');
             // delay before rebinding event to prevent retriggering
             setTimeout(bind, 700);
-            
+
             // change active class on link
             $('nav a.active').removeClass('active');
             $($('a')[currSlide]).addClass('active');
-            
+
         }
-      
+
         function bind() {
              $body.bind('false', mouseEvent);
           }
-      
+
         function mouseEvent(e, delta) {
             // On down scroll, show next slide otherwise show prev slide
             showSlide(delta >= 0 ? -1 : 1);
             e.preventDefault();
         }
-        
+
         $('nav a, .main-btn a').click(function(e) {
             // When link clicked, find slide it points to
             var newslide = parseInt($(this).attr('href')[1]);
@@ -49,25 +49,25 @@ jQuery(document).ready(function($) {
             showSlide(diff); // show that slide
             e.preventDefault();
         });
-      
+
         $(window).resize(function(){
           // Keep current slide to left of window on resize
           var displacment = window.innerWidth*currSlide;
           $slides.css('transform', 'translateX(-'+displacment+'px)');
         });
-        
+
         // cache
         var $body = $('body');
         var currSlide = 0;
         var $slides = $('.slides');
         var $slide = $('.slide');
-      
+
         // give active class to first link
         $($('nav a')[0]).addClass('active');
-        
+
         // add event listener for mousescroll
         $body.bind('false', mouseEvent);
-    })        
+    })
 
 
         $('#form-submit .date').datepicker({
@@ -85,3 +85,39 @@ jQuery(document).ready(function($) {
 
 
 });
+
+
+//Countdown
+(function () {
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+let birthday = "Feb 20, 2021 10:00:00",
+    countDown = new Date(birthday).getTime(),
+    x = setInterval(function() {
+
+      let now = new Date().getTime(),
+          distance = countDown - now;
+
+      document.getElementById("days").innerText = Math.floor(distance / (day)),
+        document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+      //do something later when date is reached
+      if (distance < 0) {
+        let headline = document.getElementById("headline"),
+            countdown = document.getElementById("countdown"),
+            content = document.getElementById("countext");
+
+        headline.innerText = "CONVENTION DAY";
+        countdown.style.display = "none";
+        content.style.display = "block";
+
+        clearInterval(x);
+      }
+      //seconds
+    }, 0)
+}());
